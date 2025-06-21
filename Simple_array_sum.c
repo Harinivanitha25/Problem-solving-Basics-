66c709bbb8cc91b2dfa57acc90e75aa159b01f15
Harini <harini.vanitha25@gmail.com>
2025-06-21 14:27:24 +0530

    Simple_array_sum.c

---
+++ Simple_array_sum.c
@@ -0,0 +1,10 @@
+// function of the simple sum of array 
+int simpleArraySum(int ar_count, int* ar) {
+   int sum=0,i;
+   for(i=0;i<ar_count;i++)
+   {
+     sum=sum+ar[i];
+     
+   }
+ return sum;
+ }
