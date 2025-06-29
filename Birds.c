09dad4fe12974fe2be9389a8f46123f913e37a9e
Harini <harini.vanitha25@gmail.com>
2025-06-29 16:09:34 +0530

    Birds.c

--- Birds.c
+++ Birds.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-int main()
-{
-   int n,count;
-   scanf("%d",&n);
-   int arr[n],i,j,max=0,value;
-   for(i=0;i<n;i++)
-     scanf("%d",&arr[i]);
-   for(i=0;i<n;i++)
-   {   count=1;
-       for(j=i+1;j<n;j++)
-       { if(arr[i]==arr[j])
-           count++;
-       }
-       if(count>max)
-        { max=count;
-          value=arr[i];
+
+#define MAX 10000  
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n], freq[MAX] = {0};
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        freq[arr[i]]++;
+    }
+
+    int max = 0, value = 0;
+    for(int i = 0; i < MAX; i++) {
+        if(freq[i] > max) {
+            max = freq[i];
+            value = i;
         }
-   }
-   printf("%d",value);
+    }
+
+    printf("%d", value);
     return 0;
 }
