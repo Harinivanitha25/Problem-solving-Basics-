1e9a4ad7db74f03325f283706210619ba2e40846
Harini <harini.vanitha25@gmail.com>
2025-07-21 18:31:19 +0530

    Add files via upload

---
+++ Long_factorial.py
@@ -0,0 +1,17 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def extraLongFactorials(n):
+    result=1
+    for i in range(2,n+1):
+        result*=i
+    print(result)
+    
+if __name__ == '__main__':
+    n = int(input().strip())
+    extraLongFactorials(n)
